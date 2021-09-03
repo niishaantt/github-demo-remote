9d0777a1f1c0a8e4d5bb6b0e4f47f04930865b52
niishaantt <90038665+niishaantt@users.noreply.github.com>
2021-09-03 16:51:52 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+#Add Implementation
+def add(x,y):
+    return x+y 
+    
+#Subtract Implementation
+def subtract(x,y):
+    return x-y   #on master branch
+
+#Multiplication Implementation
+def multiply(x,y):
+    return x*y          #on Bug456
+
+#Division Implementation
+def divide(x,y):
+    if y==0:           #on master branch
+       return DIVIDE_BY_ZERO_ERROR
+    else:
+       return x/y
+
+ 
+
